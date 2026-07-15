9c2ee41833ec5c7d415b4ecef9fe6f2928186bc3
MB16-pixel <mistedmedy@gmail.com>
2026-07-15 22:01:26 +0530

    Add result display functionality and refactor result checking logic in Tic Tac Toe game

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll("#btn");
+const res = document.getElementById("res");
 console.log(buttons) // returns a nodeList of all the buttons
 
 text = "O";
@@ -13,29 +14,41 @@ buttons.forEach((btn,index) => {
           btn.textContent = "X";
           text = "X"
           clicked[index] = "X";
+          result();
         }else{
           btn.textContent = "O";
           text = "O"
           clicked[index] = "O";
+          result();
         }
         console.log(clicked);
     });
 });
 
 function result(){
-  if(clicked[0]===clicked[1]===clicked[2]){
-    console.log("wins 1");
-  }else if(clicked[0]===clicked[3]===clicked[6]){
-    console.log("wins 2");
-  }else if(clicked[6]===clicked[7]===clicked[8]){
-    console.log("wins 3");
-  }else if(clicked[2]===clicked[6]===clicked[8]){
-    console.log("wins 4");
-  }else if(clicked[0]===clicked[4]===clicked[8]){
-    console.log("wins 5");
-  }else if(clicked[2]===clicked[4]===clicked[6]){
-    console.log("wins 6");
+  if(clicked[0]!="" && clicked[0]===clicked[1] &&clicked[1] ===clicked[2]){
+    res.textContent += `${clicked[0]} wins`;
+    document.querySelector(".board").style.pointerEvents = "none";
+  }else if(clicked[0]!="" && clicked[0]===clicked[3] &&clicked[3] ===clicked[6]){
+    res.textContent += `${clicked[0]} wins`;
+    document.querySelector(".board").style.pointerEvents = "none";
+  }else if(clicked[6]!="" && clicked[6]===clicked[7] &&clicked[7] ===clicked[8]){
+    res.textContent += `${clicked[6]} wins`;
+    document.querySelector(".board").style.pointerEvents = "none";
+  }else if(clicked[2]!="" && clicked[2]===clicked[5] &&clicked[5] ===clicked[8]){
+    res.textContent += `${clicked[2]} wins`;
+    document.querySelector(".board").style.pointerEvents = "none";
+  }else if(clicked[0]!="" && clicked[0]===clicked[4] &&clicked[4] ===clicked[8]){
+    res.textContent += `${clicked[0]} wins`;
+    document.querySelector(".board").style.pointerEvents = "none";
+  }else if(clicked[2]!="" && clicked[2]===clicked[4] && clicked[4] ===clicked[6]){
+    res.textContent += `${clicked[2]} wins`;
+    document.querySelector(".board").style.pointerEvents = "none";
+  }else if(clicked[3]!="" && clicked[3]===clicked[4] && clicked[4] === clicked[5]){
+    res.textContent += `${clicked[3]} wins`;
+    document.querySelector(".board").style.pointerEvents = "none"; // Middle Row
+  }else if(clicked[1]!="" && clicked[1]===clicked[4] && clicked[4] === clicked[7]){
+    res.textContent += `${clicked[1]} wins`;
+    document.querySelector(".board").style.pointerEvents = "none"; // Middle Column
   }
 }
-
-result();
